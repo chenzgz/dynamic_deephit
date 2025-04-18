f3ccbe1763a02ce2c474b7944c3608f82204d602
Zheng Chen <zheng-chen@hotmail.com>
2025-04-18 15:37:27 +0800

    Delete src directory

--- src/dynamic_deephit/core/__init__.py
+++
@@ -1,7 +0,0 @@
-from .main import train_dynamic_deephit_model
-from .individual_prediction import predict_with_dynamic_deephit
-from .interval_CV import interval_CV,k_fold_cross_validation,train_test_split_validation,bootstrap_validation
-from .external_validation import external_validation
-
-
-__all__ = ['train_dynamic_deephit_model', 'predict_with_dynamic_deephit','interval_CV','k_fold_cross_validation','train_test_split_validation','bootstrap_validation','external_validation']
--- src/dynamic_deephit/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .data_loader import load_test_data
-from .import_data import import_dataset
-
-
-__all__ = ['import_dataset','load_test_data']
